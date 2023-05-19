9b009576f0c572c9739488b01783993b69507d56
kongshyun <tlgus927@naver.com>
2023-05-19 17:45:45 +0900

    HI

---
+++ python/LED_pwm.py
@@ -0,0 +1,20 @@
+import RPi.GPIO as GPIO
+import time
+
+def led_pwm(pin):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT,initial=GPIO.LOW)
+
+    pwm = GPIO.PWM(pin, 100) # pwm 설정 (100Hz)
+    pwm.start(0)
+    
+    # 밝기가 0에서 100 까지 0.1초 간격으로 바뀜
+    for i in range(101):
+        pwm.ChangeDutyCycle(i)
+        time.sleep(0.1)
+    
+    pwm.stop()
+    GPIO.cleanup()
+
+led_pwm(17)
+GPIO.cleanup()
